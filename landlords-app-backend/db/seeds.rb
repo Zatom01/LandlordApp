b818a1a18b89c23a626010decddd2f392af8737c
Anil <anil_gurung15@yahoo.com>
2020-10-13 13:38:14 -0700

    test

---
+++ landlords-app-backend/db/seeds.rb
@@ -0,0 +1,46 @@
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
+
+# Blaze = {
+#     name: Blaze,
+#     age: 30,
+#     state: Nevada,
+#     city: Reno,
+#     number_of_houses: 5
+
+# }
+
+# Neal = {
+#     name: Neal,
+#     age: 29,
+#     state: Nevada,
+#     city: Reno,
+#     number_of_houses: 6
+
+# }
+
+# Ben = {
+#     name: Ben,
+#     age: 26,
+#     state: Nevada,
+#     city: Reno,
+#     number_of_houses: 3
+
+# }
+
+# Alina = {
+#     name: Alina,
+#     age: 29,
+#     state: Nevada,
+#     city: Reno,
+#     number_of_houses: 9
+
+# }
+
+
+# User.create([Blaze])
