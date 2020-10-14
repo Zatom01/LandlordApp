5b62d68dd1fca83c8e27e3f3945ff448ddf8f1d8
Anil <anil_gurung15@yahoo.com>
2020-10-14 13:00:46 -0700

    added reducers, created store with root reducer

---
+++ landlords-app-frontend/src/reducers/landLordsReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    loading: true,
+    landlords: []
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+
+    }
+}
